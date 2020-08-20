8c665df01075b58a51c1d32e8856dd73f1ed9575
Yee Hung <19034609@myrp.edy.sg>
2020-08-20 21:59:57 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/c346/id19034609/demoreceiverapp/BatteryReceiver.java
@@ -0,0 +1,17 @@
+package sg.edu.c346.id19034609.demoreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BatteryReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast my_toast = Toast.makeText(context,"Low Battery", Toast.LENGTH_LONG);
+        my_toast.show();
+
+    }
+}
